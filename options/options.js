e4b3279c8ffa19c71a84b1237a0047a251ae3d2b
Andre Machado <andre.machado@crowdbotics.com>
2023-08-13 12:15:22 -0400

    [Crowdbotics Platform Commit]
    Crowdbotics Studio: Update app screens

--- options/options.js
+++ options/options.js
@@ -1,10 +1,11 @@
 export const globalOptions = {
-  name: "komatsuhattest-42823",
-  url: "https://komatsuhattest-42823.botics.co",
-  api: "https://komatsuhattest-42823.botics.co/api/v1",
-  screenOptions: {
-    headerShown: false
-  }
+  "name": "komatsuhattest-42823",
+  "url": "https://komatsuhattest-42823.botics.co",
+  "api": "https://komatsuhattest-42823.botics.co/api/v1",
+  "screenOptions": {
+    "headerShown": false
+  },
+  "initialRoute": "ScreenAI2"
 };
 export const modulesOptions = {
   "@modules/app-menu": {
